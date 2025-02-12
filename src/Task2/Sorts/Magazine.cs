ac56d59ec35a4511cf8b393df5ce465720cf5f8a
andrmda6014 <lappter2@mail.ru>
2025-02-12 16:05:25 +0700

    Add files via upload

---
+++ src/Task2/Sorts/Magazine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Interface;
+
+namespace Task2.Sorts
+{
+    public class Magazine : Item, ILendable
+    {
+        public bool IsAvailable { get; private set; }
+
+        public Magazine(string title, int publicationYear) : base(title, publicationYear)
+        {
+            IsAvailable = true;
+        }
+
+        public void Lend()
+        {
+            IsAvailable = false;
+        }
+
+        public void Return()
+        {
+            IsAvailable = true;
+        }
+    }
+}
---
+++ src/Task2/Sorts/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Sorts
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        protected Person(string name, DateTime dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public abstract void DisplayInfo();
+    }
+}

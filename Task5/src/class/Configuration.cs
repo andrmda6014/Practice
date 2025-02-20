e46e9b2cfad84dbbcdd82fc1836bacb6656c9a35
andrmda6014 <lappter2@mail.ru>
2025-02-20 17:33:32 +0700

    Часть 4
    Добавьте в вывод меню условия, которые будут проверять наличие данных в структурах и при отсутствии данных не будут выводить пункты меню (или отмечать, что пункты не доступны) и при вводе номера пункта меню вместо обработки пункта будет выводиться сообщение о том, что нет данных для выполнения пункта.

---
+++ Task5/src/class/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class Device
+    {
+        public string Name { get; set; } // Имя устройства
+    }
+    public class Configuration
+    {
+        public Dictionary<string, Device> Devices { get; set; } // Словарь для хранения устройств
+    }
+}
